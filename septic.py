b4ebd1fe511fb0c7ac2d86b3ed966c444c696f92
wolfymc <45526195+wolfymc@users.noreply.github.com>
2018-12-02 14:27:47 -0800

    Add files via upload

---
+++ septic.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+TOKEN = 'NTE4OTExOTMzMzk5OTU3NTA1.DuXqNg._SMERP59XVVHSvhtmOiz_L5desk'
+client = commands.Bot(command_prefix = 's!')
+
+extensions = ['fun', 'member']
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=discord.Game(name='s!help'))
+    print('Bot is ready')
+	
+@client.command()
+async def load(extension):
+	try:
+		client.load_extension(extension)
+		print('Loaded {}'.format(extension))
+	except Exception as error:
+		print('{} cannot be loaded. [{}]'.format(extension, error))
+		
+@client.command()
+async def unload(extension):
+	try:
+		client.unload_extension(extension)
+		print('Unloaded {}'.format(extension))
+	except Exception as error:
+		print('{} cannot be unloaded. [{}]'.format(extension, error))
+	
+if __name__ == '__main__':
+	for extension in extensions:
+		try:
+			client.load_extension(extension)
+		except Exception as error:
+			print('{} cannot be loaded. [{}]'.format(extension, error))
+	
+	client.run(TOKEN)
